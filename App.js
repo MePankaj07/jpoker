d8960755060ed91147aef8e5b4f9db42baa72882
Pankaj Bhardwaj <pankajbhardwaj994@gmail.com>
2022-05-20 21:44:38 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, StatusBar, Image } from 'react-native';
+import React from 'react';
+
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar
+        animated={true}
+        backgroundColor="#000"
+      />
+      <View height='1%' />
+      <View style={{
+        backgroundColor: '#20252b', width: '100%', height: '6%', flexDirection: "row",
+
+        paddingHorizontal: 10
+      }}>
+        <Image source={require('./assets/logo.png')} style={{
+          width: 65,
+          height: 40,
+
+        }} />
+
+        <Text style={{ color: '#fff', fontSize: 22, paddingTop: 7, paddingHorizontal: 10, fontFamily: 'Roboto', fontWeight: "600" }}>
+          Hey Shubham !</Text>
+
+
+      </View>
+
+      <View height='2%' />
+      <Text style={{ color: '#fff', fontSize: 22, paddingTop: 7, paddingHorizontal: 10, fontFamily: 'Roboto', fontWeight: "600" }}>
+        Hey Shubham !</Text>
+      <View height='2%' />
+
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#000',
   },
+
 });
